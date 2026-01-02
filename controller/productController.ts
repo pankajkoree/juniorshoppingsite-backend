7d83dc728b7975d7b0b145ba47b51ab79d1f4814
pankajkoree <pankajkori2057@gmail.com>
2026-01-02 19:37:56 +0545

    api -> products by category

--- controller/productController.ts
+++ controller/productController.ts
@@ -4,7 +4,39 @@ import prisma from "../DB/db.config"
 export const getProducts = async (req: Request, res: Response) => {
     try {
         const products = await prisma.product.findMany()
+
+        // <------- response for successfull ------->
         return res.status(200).json({ success: true, message: "successfully fetched all products", data: products })
+    } catch (error) {
+        // <------- response for server error ------->
+        return res.status(500).json({ message: "server error", error: error })
+    }
+}
+
+export const getProductsByCategory = async (req: Request, res: Response) => {
+    try {
+        const category = req.params.category
+        const fetchedProducts = await prisma.product.findMany({
+            where: { category: category },
+            select: {
+                id: true,
+                title: true,
+                description: true,
+                price: true,
+                discountPercentage: true,
+                rating: true,
+                brand: true,
+                thumbnail: true,
+            }
+        })
+
+        // <------- response if no product exists with entered category ------->
+        if (fetchedProducts.length === 0) {
+            return res.status(404).json({ success: false, message: `no products for ${category}` })
+        }
+
+        // <------- response for success ------->
+        return res.status(201).json({ success: true, total: fetchedProducts.length, data: fetchedProducts })
     } catch (error) {
         // <------- response for server error ------->
         return res.status(500).json({ message: "server error", error: error })
--- routes/productRoute.ts
+++ routes/productRoute.ts
@@ -1,8 +1,9 @@
 import { Router } from "express"
-import { getProducts } from "../controller/productController"
+import { getProducts, getProductsByCategory } from "../controller/productController"
 
 const router = Router()
 
 router.get("/", getProducts)
+router.post("/category/:category", getProductsByCategory)
 
 export default router
