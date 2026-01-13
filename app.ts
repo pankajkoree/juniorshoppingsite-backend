fdc9c73b1b2005db5fac43be6f997bc22118e2aa
pankajkoree <pankajkori2057@gmail.com>
2026-01-13 21:36:26 +0545

    cors updated

--- app.ts
+++ app.ts
@@ -1,13 +1,27 @@
 import express from "express"
 import dotenv from "dotenv"
 import { Request, Response } from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 4000
 
+app.use(cors({
+    origin: [
+        "http://localhost:3000",
+        "https://juniorshoppingsite-backend-1.onrender.com"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+
+}))
+
 app.use(express.json())
+app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
 
 // Routes
